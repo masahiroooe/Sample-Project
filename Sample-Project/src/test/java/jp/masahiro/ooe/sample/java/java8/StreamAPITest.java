df5f8b7ff75f86a3419eaac2d6ffcbb69aa905b7
masahiro <masahiro@mooe-home>
2017-11-04 20:48:38 +0900

    ソート例追加

--- Sample-Project/src/test/java/jp/masahiro/ooe/sample/java/java8/StreamAPITest.java
+++ Sample-Project/src/test/java/jp/masahiro/ooe/sample/java/java8/StreamAPITest.java
@@ -3,9 +3,12 @@ package jp.masahiro.ooe.sample.java.java8;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.junit.Test;
+
+import jp.masahiro.ooe.sample.java.java8.bean.TestBean;
 
 public class StreamAPITest {
 
@@ -21,7 +24,6 @@ public class StreamAPITest {
 
 		List<String> returnList = testList.stream().distinct().collect(Collectors.toList());
 
-
 		returnList.stream().forEach(System.out::println);
 
 		System.out.println("\n-- sorted --\n");
@@ -34,12 +36,44 @@ public class StreamAPITest {
 	public void test2() throws Exception {
 		List<String> testList = Arrays.asList("zzz", "aaa", "bbb", "ccc", "aaa", "bbb");
 
-
-
 		List<String> returnList = testList.stream().sorted(Comparator.naturalOrder()).distinct()
 				.collect(Collectors.toList());
 
 		returnList.stream().sorted(Comparator.naturalOrder()).forEach(System.out::println);
 
 	}
+
+	private Function<String, TestBean> getTestBean = new Function<String, TestBean>() {
+		@Override
+		public TestBean apply(String arg0) {
+			TestBean testBean = new TestBean();
+			testBean.setId(arg0);
+			testBean.setStrList(Arrays.asList("zzz", "aaa", "bbb", "ccc", "aaa", "bbb"));
+			return testBean;
+		}
+	};
+
+	private Function<List<String>, List<TestBean>> getTestBeanList = new Function<List<String>, List<TestBean>>() {
+
+		@Override
+		public List<TestBean> apply(List<String> t) {
+			return t.stream().map(act -> getTestBean.apply(act)).collect(Collectors.toList());
+		}
+
+	};
+
+	/**
+	 * BeanのIDでリストを作成<br>
+	 * リストはIDの重複なし、逆順ソート
+	 */
+	@Test
+	public void test3() throws Exception {
+		List<TestBean> testList = getTestBeanList.apply(Arrays.asList("zzz", "aaa", "bbb", "ccc", "aaa", "bbb"));
+		List<String> retList = testList.stream().sorted(Comparator.comparing(TestBean::getId).reversed()).distinct()
+				.map(map -> map.getId()).collect(Collectors.toList());
+
+		retList.stream().forEach(System.out::println);
+
+	}
+
 }
--- Sample-Project/src/test/java/jp/masahiro/ooe/sample/java/java8/bean/TestChildBean.java
+++ Sample-Project/src/test/java/jp/masahiro/ooe/sample/java/java8/bean/TestChildBean.java
@@ -2,6 +2,9 @@ package jp.masahiro.ooe.sample.java.java8.bean;
 
 import java.util.List;
 
+import lombok.Data;
+
+@Data
 public class TestChildBean {
 
 	private String valueStr;
@@ -9,29 +12,5 @@ public class TestChildBean {
 	private Integer valueInt;
 
 	private List<String> valueListStr;
-
-	public String getValueStr() {
-		return valueStr;
-	}
-
-	public void setValueStr(String valueStr) {
-		this.valueStr = valueStr;
-	}
-
-	public Integer getValueInt() {
-		return valueInt;
-	}
-
-	public void setValueInt(Integer valueInt) {
-		this.valueInt = valueInt;
-	}
-
-	public List<String> getValueListStr() {
-		return valueListStr;
-	}
-
-	public void setValueListStr(List<String> valueListStr) {
-		this.valueListStr = valueListStr;
-	}
 
 }
